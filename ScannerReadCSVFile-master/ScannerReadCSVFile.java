68035b8a99bb04448b5a78fc329f847f153821d9
bowie2024 <113475706+bowie2024@users.noreply.github.com>
2022-12-01 13:50:14 -0800

    Add files via upload

---
+++ ScannerReadCSVFile-master/ScannerReadCSVFile.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Average each row of ten test scores
+ */
+public class ScannerReadCSVFile
+{
+    public void run() throws FileNotFoundException {
+        // Mr. Jaffe's code to read data from a file
+        // Instantiate a File object
+        File dataFile = new File("TestScoresByClass.csv");
+
+        // Instantiate a Scanner object that uses the file
+        Scanner scanner = new Scanner(dataFile);
+
+        // Set the delimiter as a new-line character so we can read the
+        // data one line at a time
+        scanner.useDelimiter("\n");
+
+        // Continue while there's still data in the file to be read
+        while (scanner.hasNext()) {
+            // Read the next line of the file
+            String line = scanner.nextLine();
+            String[] array = line.split(",");
+            int c = 0;
+            String sect = "";
+            for (int i = 0; i < array.length; i++) {
+                if (i == 0){
+                    sect = array[i];
+                }
+                else {
+                    int b = Integer.parseInt(array[i]);
+                    c = (c + b);
+                }
+            }
+            c = (c/10);
+            String out = (sect + ": " + c);
+            System.out.println(out);
+        }
+    }    
+
+    public static void main (String[] args) {
+        try {
+            ScannerReadCSVFile srCsv = new ScannerReadCSVFile();
+            srCsv.run();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (Exception e) {
+            System.out.println("There's an error someplace. Try using the debugger to find it!");
+
+        }
+    }
+}
+
